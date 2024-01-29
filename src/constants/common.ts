6acb0926765bd75630368fcfda293b968609c790
ThangNguyen <101063068+thangdevalone@users.noreply.github.com>
2024-01-29 11:44:22 +0700

    Update common.ts

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,2 +1,3 @@
-export const STATIC_HOST = "https://thangdev.online/"
+
+export const STATIC_HOST = "https://server-iot-food-04.onrender.com/"
 export const SOCKET_URL = "https://thangdev.online/ws-iotfood"
